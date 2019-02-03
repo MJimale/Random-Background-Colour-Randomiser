d9aa7fbcca3528a7f55fa733f13fdac5c481e2ee
Mustafa <mustafa1996.mj@gmail.com>
2019-02-03 08:29:18 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import rainbow from './Components/rainbow.png'
+import Random from "./Components/Random.js";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img
+          src={rainbow}
+          alt="Rainbow"
+        />
+        <h1>React Random Colour Generator</h1>
+        <Random />
+        <p>Click the "Refresh" Button to see some magic happen!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
